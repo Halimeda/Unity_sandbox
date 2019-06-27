4ab07c2226deafd1ed1981dcc924ceb9e5e9f0b2
Hélène DC <h.decock92@gmail.com>
2019-06-27 16:48:50 +0200

    add Gui

---
+++ Animation_exo/Assets/Scripts/CubeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("StartMoving", true);
+        }
+        
+    }
+}
---
+++ Animation_exo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void StratGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void DisplaySliderValue(float value)
+    {
+        Debug.Log("Slider Value: " + value.ToString());
+    }
+
+}
