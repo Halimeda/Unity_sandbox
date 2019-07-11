5eb4bbdd29f484db40557c8954535d001b04f93a
Hélène DC <h.decock92@gmail.com>
2019-07-11 16:04:21 +0200

    add sound scene to animation exo

---
+++ Animation_exo/Assets/Scripts/AudioSnapshotTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSnapshotTransition : MonoBehaviour
+{
+
+    public AudioMixerSnapshot snapshot;
+    public AudioMixerSnapshot snapshot1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            snapshot.TransitionTo(5f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        snapshot1.TransitionTo(5f);
+    }
+
+}
---
+++ Animation_exo/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    AudioSource sound;
+    private bool isPickedUp = false; //Not play the sound a second time
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !isPickedUp)
+        {
+            sound.Play();
+            Destroy(this.gameObject, 1f);
+            isPickedUp = true;
+        }
+    }
+
+    //externe audio source
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Destroy(this.gameObject);
+    //}
+}
